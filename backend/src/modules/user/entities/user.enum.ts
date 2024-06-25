6eacc788452ebc071e168a9a0c68a90e41e34dd0
millanjuan <millanj159951@gmail.com>
2024-06-25 17:29:07 -0300

    User entity

---
+++ backend/src/modules/user/entities/user.enum.ts
@@ -0,0 +1,16 @@
+export enum Gender {
+  MASCULINO = 'Masculino',
+  FEMENINO = 'Femenino',
+  OTRO = 'Otro',
+}
+
+export enum IdentityType {
+  CIUDADANIA = 'Cédula de ciudadanía',
+  EXTRANJERIA = 'Cédula de extranjería',
+}
+
+export enum UserRole {
+  ADMINISTRADOR = 'Administrador',
+  USUARIO = 'Usuario',
+  MODERADOR = 'Moderador',
+}
